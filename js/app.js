8c9bfde0c091f12712546942f42694701041aab3
Jen Jayme <jeniferjayme@gmail.com>
2020-10-21 00:30:13 -0700

    Added basic pages, navigation framework

--- js/app.js
+++ js/app.js
@@ -1,1 +1,17 @@
 $(document).foundation();
+  
+  $( document ).ready(function() {
+    console.log( "ready!" );
+    $(function() {
+      $(window).scroll(function() {
+        didScroll = true;
+        var winTop = $(window).scrollTop();
+        if (winTop >= 30) {
+          $("body").addClass("sticky-shrinknav-wrapper");
+        } else{
+          $("body").removeClass("sticky-shrinknav-wrapper");
+        }
+      });
+    });
+});
+  
